57a2ada9a531ea629f3ec82a25399198ca35714a
MaxenceThacker <maxence.thacker@hotmail.fr>
2021-10-12 17:20:02 +0200

    12/10/2021 17:20:02,49

---
+++ Base de donnée/Exercices/Exercice 7 - Bibliotheque/scriptBiblio.sql
@@ -0,0 +1,194 @@
+DROP DATABASE IF EXISTS "Ajouter le nom de la base";
+CREATE DATABASE IF NOT EXISTS "Ajouter le nom de la base";
+USE "Ajouter le nom de la base";
+
+--
+--  EtatsReserves
+--
+
+CREATE TABLE EtatsReserves(
+   IdEtatReserve INT AUTO_INCREMENT  PRIMARY KEY,
+   LibelleEtatReserve VARCHAR(50)
+) ENGINE=InnoDB;
+
+--
+--  Themes
+--
+
+CREATE TABLE Themes(
+   IdTheme INT AUTO_INCREMENT PRIMARY KEY,
+   NomTheme VARCHAR(50)
+)ENGINE=InnoDB;
+
+--
+--  Auteurs
+--
+
+CREATE TABLE Auteurs(
+   IdAuteur INT AUTO_INCREMENT PRIMARY KEY,
+   NomAutheur VARCHAR(50)
+)ENGINE=InnoDB;
+
+--
+--  Genres
+--
+
+CREATE TABLE Genres(
+   IdGenre INT AUTO_INCREMENT PRIMARY KEY,
+   NomGenre VARCHAR(50) ,
+   PRIMARY KEY(IdGenre)
+)ENGINE=InnoDB;
+
+--
+--  Editeurs
+--
+
+CREATE TABLE Editeurs(
+   IdEditeur INT AUTO_INCREMENT PRIMARY KEY,
+   NomEditeur VARCHAR(50) ,
+   PRIMARY KEY(IdEditeur)
+)ENGINE=InnoDB;
+
+--
+--  Usures
+--
+
+CREATE TABLE Usures(
+   IdUsure INT AUTO_INCREMENT PRIMARY KEY,
+   CodeUsure VARCHAR(50) ,
+   PRIMARY KEY(IdUsure)
+)ENGINE=InnoDB;
+
+--
+--  CategoriesProfessionnelles
+--
+
+CREATE TABLE CategoriesProfessionnelles(
+   IdCategorieProfessionnelle INT AUTO_INCREMENT PRIMARY KEY,
+   LibelleCategPro VARCHAR(50) ,
+   PRIMARY KEY(IdCategorieProfessionnelle)
+)ENGINE=InnoDB;
+
+--
+--  MotsCles
+--
+
+CREATE TABLE MotsCles(
+   IdMotCle INT AUTO_INCREMENT PRIMARY KEY,
+   LibelleMotCle VARCHAR(50) ,
+   PRIMARY KEY(IdMotCle)
+)ENGINE=InnoDB;
+
+--
+--  Livres
+--
+
+CREATE TABLE Livres(
+   IdLivre INT AUTO_INCREMENT PRIMARY KEY,
+   TitreLivre VARCHAR(50) ,
+   CodeCatalogue VARCHAR(50) ,
+   IdEditeur INT NOT NULL,
+   IdTheme INT NOT NULL,
+   PRIMARY KEY(IdLivre),
+   FOREIGN KEY(IdEditeur) REFERENCES Editeurs(IdEditeur),
+   FOREIGN KEY(IdTheme) REFERENCES Themes(IdTheme)
+)ENGINE=InnoDB;
+
+--
+--  Exemplaires
+--
+
+CREATE TABLE Exemplaires(
+   IdExemplaire INT AUTO_INCREMENT PRIMARY KEY, 
+   DateAcquisition DATE,
+   Disponibilite BOOLEAN,
+   CodeRayon VARCHAR(50) ,
+   IdUsure INT NOT NULL,
+   IdLivre INT NOT NULL,
+   PRIMARY KEY(IdExemplaire),
+   FOREIGN KEY(IdUsure) REFERENCES Usures(IdUsure),
+   FOREIGN KEY(IdLivre) REFERENCES Livres(IdLivre)
+)ENGINE=InnoDB;
+
+--
+--  Abonnes
+--
+
+CREATE TABLE Abonnes(
+   IdAbonne INT AUTO_INCREMENT PRIMARY KEY,
+   MatriculeAbonne VARCHAR(50) ,
+   NomAbonne VARCHAR(50) ,
+   AdresseAbonne VARCHAR(50) ,
+   TelephoneAbonne VARCHAR(50) ,
+   DateAdhesion DATE,
+   DateNaissance DATE,
+   IdCategorieProfessionnelle INT NOT NULL,
+   PRIMARY KEY(IdAbonne),
+   FOREIGN KEY(IdCategorieProfessionnelle) REFERENCES CategoriesProfessionnelles(IdCategorieProfessionnelle)
+)ENGINE=InnoDB;
+
+--
+--  Reservations
+--
+
+CREATE TABLE Reservations(
+   IdEtatReserve INT,
+   IdLivre INT,
+   IdAbonne INT,
+   DateDebutReservation DATETIME,
+   DateDemandeReservation DATE,
+   PRIMARY KEY(IdEtatReserve, IdLivre, IdAbonne),
+   FOREIGN KEY(IdEtatReserve) REFERENCES EtatsReserves(IdEtatReserve),
+   FOREIGN KEY(IdLivre) REFERENCES Livres(IdLivre),
+   FOREIGN KEY(IdAbonne) REFERENCES Abonnes(IdAbonne)
+)ENGINE=InnoDB;
+
+--
+--  Definitions
+--
+
+CREATE TABLE Definitions(
+   IdGenre INT,
+   IdLivre INT,
+   PRIMARY KEY(IdGenre, IdLivre),
+   FOREIGN KEY(IdGenre) REFERENCES Genres(IdGenre),
+   FOREIGN KEY(IdLivre) REFERENCES Livres(IdLivre)
+)ENGINE=InnoDB;
+
+--
+--  Compositions
+--
+
+CREATE TABLE Compositions(
+   IdAuteur INT,
+   IdLivre INT,
+   PRIMARY KEY(IdAuteur, IdLivre),
+   FOREIGN KEY(IdAuteur) REFERENCES Auteurs(IdAuteur),
+   FOREIGN KEY(IdLivre) REFERENCES Livres(IdLivre)
+)ENGINE=InnoDB;
+
+--
+--  EmpruntExemplaires
+--
+
+CREATE TABLE EmpruntExemplaires(
+   IdExemplaire INT,
+   IdAbonne INT,
+   DateEmprunt DATETIME,
+   DateRetourEffective DATETIME,
+   PRIMARY KEY(IdExemplaire, IdAbonne),
+   FOREIGN KEY(IdExemplaire) REFERENCES Exemplaires(IdExemplaire),
+   FOREIGN KEY(IdAbonne) REFERENCES Abonnes(IdAbonne)
+)ENGINE=InnoDB;
+
+--  Identifications
+--
+
+CREATE TABLE Identifications(
+   IdLivre INT,
+   IdMotCle INT,
+   PRIMARY KEY(IdLivre, IdMotCle),
+   FOREIGN KEY(IdLivre) REFERENCES Livres(IdLivre),
+   FOREIGN KEY(IdMotCle) REFERENCES MotsCles(IdMotCle)
+)ENGINE=InnoDB;
+
