156623fc017041ac3e62c34c9c2a5fde7387ffb4
59011-07-11 <59011-07-11@exchange.ad.afpanet>
2021-10-25 12:23:49 +0200

    25/10/2021 12:23:49,60

--- Script/saveBase.bat
+++ Script/saveBase.bat
@@ -3,6 +3,6 @@ SetLocal EnableDelayedExpansion
 cd C:\wamp64\bin\mysql\mysql5.7.31\data
 MKDIR U:\59011-07-11\Sauvegardebdd\%DATE:~6,4%%DATE:~3,2%%DATE:~0,2%
 for /d %%i in (*) do (
-if /I %%i NEQ performance_schema if /I %%i NEQ mysql if /I %%i NEQ sys C:\wamp64\bin\mysql\mysql5.7.31\bin --user=root --databases %%i > U:\59011-07-11\Sauvegardebdd\%DATE:~6,4%%DATE:~3,2%%DATE:~0,2%\backup_%%i_%DATE:~6,4%%DATE:~3,2%%DATE:~0,2%.sql  
+if /I %%i NEQ performance_schema if /I %%i NEQ mysql if /I %%i NEQ sys C:\wamp64\bin\mysql\mysql5.7.31\bin\mysqldump --user=root --databases %%i > U:\59011-07-11\CDA\Sauvegardebdd\Backup_Du_%DATE:~0,2%"-"%DATE:~3,2%"-"%DATE:~6,4%\____%%i____%DATE:~0,2%"-"%DATE:~3,2%"-"%DATE:~6,4%.sql  
 )
 EndLocal
