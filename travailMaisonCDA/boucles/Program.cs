9a23b2f765f667709eb12e582693e73d382f4a55
MaxenceThacker <82573979+MaxenceThacker@users.noreply.github.com>
2021-11-02 17:40:17 +0100

    Add files via upload

--- travailMaisonCDA/boucles/Program.cs
+++ travailMaisonCDA/boucles/Program.cs
@@ -59,8 +59,217 @@ namespace boucles
                 }
                 Console.WriteLine("");
             }
-            */
             
+            //Exercice 8 
+
+            int x, n;
+            int total = 1;
+            int i;
+            bool boolean;
+
+            do
+            {
+
+                Console.WriteLine("entrer le nombre ");
+                x = int.Parse(Console.ReadLine());
+                Console.WriteLine("entrer la puissance ");
+                boolean = int.TryParse(Console.ReadLine(), out n);
+
+            } while (!boolean || n < 0);
+
+            for (i = 1; i <= n; i++)
+             {
+                    total = total * x;
+             }
+             Console.WriteLine("total de l'operation " + total);
+            
+             //Exercice 9
+
+            int n;
+            Console.Write("Saisissez une valeur:");
+            n = int.Parse(Console.ReadLine());          
+            for ( int i=0; i<n;i++)
+            {
+             Console.WriteLine();
+             for (int j = 0; j < n; j++)
+             {
+              Console.Write("X ");
+             }
+            }
+            */
+            //Exercice 10
+
+            int val1;
+            int val2;
+            string ope;
+            demanderEntierPositif(string texte);
+            {
+                int n;
+                bool boolean;
+                do
+                {
+                    Console.WriteLine(texte);
+                    boolean = int.TryParse(Console.ReadLine(), out n);
+                } while (!boolean || n < 0);
+                return n;
+            }
+            demanderDoubleNonNul(string texte);
+            {
+                double n;
+                bool conversionReussi;
+                do
+                {
+                    Console.WriteLine(texte);
+                    conversionReussi = int.TryParse(Console.ReadLine(), out n);
+                } while (!conversionReussi || n < 0);
+                return n;
+            }
+            demanderDouble();
+            {
+                float n;
+                bool boolean;
+                do
+                {
+                    Console.WriteLine("entrer le nombre ");
+                    boolean = int.TryParse(Console.ReadLine(), out n);
+                } while (!boolean);
+                return n;
+            }
+            demanderOperateur();
+            {
+                char n;
+                bool boolean;
+                do
+                {
+                    Console.WriteLine("entrer le opérateur :");
+                    boolean = char.TryParse(Console.ReadLine(), out n);
+                } while (!boolean || n = '+' || n = '-' || n = '/' || n = '*' || n = '$' || n = '!' || n = 'V');
+                return n;
+            }
+            demanderCalcul();
+            {
+                double n;
+                char ope;
+                double n1;
+                int
+                demanderRelatif(n);
+                demanderOperateur(ope);
+                switch (ope)
+                {
+                    case "-":
+                        demanderRelatif()
+                    Console.Write()
+                }
+            }
+            demanderCalculSimple();
+            {
+                char n;
+                bool boolean;
+                do
+                {
+                    Console.WriteLine("entrer le opérateur :");
+                    boolean = char.TryParse(Console.ReadLine(), out n);
+                } while (!boolean || n = '+' || n = '-' || n = '/' || n = '*' || n = '$' || n = '!' || n = 'V');
+                return n;
+            }
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////
+            public static double demanderDoubleNonNull(string texte)
+            {
+                double n;
+                bool conversionReussie;
+                do
+                {
+                    Console.WriteLine(texte);
+                    conversionReussie = double.TryParse(Console.ReadLine(), out n);
+                } while (!conversionReussie || n != 0);
+                return n;
+            }
+            public static int demanderEntierPositif(string texte)
+            {
+                int valeur;
+                bool conversionReussie;
+                do
+                {
+                    Console.WriteLine(texte);
+                    conversionReussie = int.TryParse(Console.ReadLine(), out valeur);
+
+                } while (!conversionReussie || valeur < 0);
+                return valeur;
+            }
+            static double demanderDouble(string texte)
+            {
+                double nb;
+                bool ok;
+                do
+                {
+                    Console.Write(texte);
+                    ok = double.TryParse(Console.ReadLine(), out nb);
+                } while (!ok);
+                return nb;
+            }
+            static char DemanderOperateur()
+            {
+                bool ok = true;
+                char op;
+                bool condition;
+
+                do
+                {
+                    Console.Write("Entrez un opérateur +,-,*,/,$,!,V : ");
+                    ok = char.TryParse(Console.ReadLine(), out op);
+                    condition = !ok || (op != '+' && op != '-' && op != '*' && op != '/' && op != '$' && op != '!' && op != 'V' && op != 'v');
+                    if (condition)
+                        Console.WriteLine("Saisie incorrecte.");
+
+                } while (condition);
+                return char.ToUpper(op);
+            }
+            static double calculSimple(double valeur1, char operateur, double valeur2)
+            {
+                double resultat;
+                switch (operateur)
+                {
+                    case '+':
+                        resultat = valeur1 + valeur2;
+                        break;
+                    case '-':
+                        resultat = valeur1 - valeur2;
+                        break;
+                    case '*':
+                        resultat = valeur1 * valeur2;
+                        break;
+                    case '/':
+                        resultat = valeur1 / valeur2;
+                        break;
+                    case '$':
+                        resultat = Math.Pow(valeur1, valeur2);
+                        break;
+                    default:
+                        break;
+                }
+              return resultat;
+            }
+            static double calcul(double valeur1, char operateur, double valeur2)
+            {
+                double resultat;
+                switch (operateur)
+                {
+                    case '!':
+                        resultat = valeur1 + valeur2;
+                        break;
+                    case 'v':
+                        resultat = valeur1 - valeur2;
+                    default:
+                        break;
+                }
+              return resultat;
+            }
+
+
+
+
+
+
 
 
         }
