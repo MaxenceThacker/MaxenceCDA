4fbc73924f27c7d64a899d2fc1186cc9d4e0dec9
MaxenceThacker <82573979+MaxenceThacker@users.noreply.github.com>
2021-11-02 14:13:33 +0100

    Add files via upload

---
+++ travailMaisonCDA/boucles/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace boucles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*    
+            //Exercice 4
+             int n;
+
+            Console.Write("Veuillez rentrez une valeur :");
+            n = (int.Parse(Console.ReadLine()));
+            for (int i = n; i > 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+       
+            //Exercice 5
+            int n;
+            int i = 1;
+            int fact = 1;
+            Console.Write("Veuillez entrez une valeur :");
+            n = int.Parse(Console.ReadLine());
+            do
+            {
+                fact *= i++;
+            }
+            while (i <= n)
+            Console.WriteLine("La factorielle de " + n + "est" + fact);
+
+            //Exercice 6 
+
+            int nb;
+            bool val;
+
+
+            do
+            {
+                Console.Write("Entrez une valeur entière : ");
+                val = int.TryParse(Console.ReadLine(), out nb);
+            } while (!val || nb < 0);
+
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(nb + " x " + i + " = " + (nb * i));
+            }
+
+
+            //Exercice 7
+            int valeur;
+            for(int i=1;i <= 10;i++)
+            {
+                for(int j=1;j <= 10;j++)
+                {
+                    valeur = i * j;
+                    Console.Write(valeur + "," );
+                }
+                Console.WriteLine("");
+            }
+            */
+            
+
+
+        }
+    }
+}
+
