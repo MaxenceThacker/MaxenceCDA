00b53d66a1b779a06b90f258578a57dbf919d21f
Maxence Thacker <maxence.thacker@hotmail.fr>
2021-11-05 10:50:03 +0100

    05/11/2021 10:50:02,45

---
+++ Composants d'interface/Interface Desktop/Développer en C#/POO/1 - Exercice_Classe/ExerciceClasse_1/ExerciceClasse_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExerciceClasse_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Voitures citroenC4 = new Voitures()
+        }
+    }
+}
---
+++ Composants d'interface/Interface Desktop/Développer en C#/POO/1 - Exercice_Classe/ExerciceClasse_1/ExerciceClasse_1/Voitures.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciceClasse_1
+{
+    class Voitures
+    {
+        public string Couleur { get; set; }
+        public string Marque { get; set; }
+        public string Modele { get; set; }
+        public int NbKilometres { get; set; }
+        public string Motorisation { get; set; }
+
+        public Voitures(string couleur, string marque, string modele, int nbKilometres, string motorisation)
+        {
+            Couleur = couleur;
+            Marque = marque;
+            Modele = modele;
+            NbKilometres = nbKilometres;
+            Motorisation = motorisation;
+        }
+
+        public Voitures(string marque, string modele)
+        {
+            Marque = marque;
+            Modele = modele;
+        }
+
+        public override string ToString()
+        {
+            return "Cette voiture est une " + this.Modele + " de la marque " + this.Marque + " de couleur " + this.Couleur + " de motorisation " + this.Motorisation + " avec Nb de kilomètres " + this.NbKilometres + ".";
+        }
+    }
+}
