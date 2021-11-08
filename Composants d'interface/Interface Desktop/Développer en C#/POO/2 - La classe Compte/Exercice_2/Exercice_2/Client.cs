1d2e52cc71c740dbb56f0100093f19dd52b45b74
Maxence Thacker <maxence.thacker@hotmail.fr>
2021-11-08 12:20:10 +0100

    08/11/2021 12:20:06,14

---
+++ Composants d'interface/Interface Desktop/Développer en C#/POO/2 - La classe Compte/Exercice_2/Exercice_2/Client.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice_2
+{
+    class Client
+    {
+        public int Cin { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public int Telephone { get; set; }
+
+        public Client(int cin, string nom, string prenom, int telephone)
+        {
+            Cin = cin;
+            Nom = nom;
+            Prenom = prenom;
+            Telephone = telephone;
+        }
+        public Client(int cin, string nom, string prenom)
+        {
+            Cin = cin;
+            Nom = nom;
+            Prenom = prenom;
+        }
+        public string Afficher()
+        {
+            return "Les informations du Client sont :" + 
+                   "CIN : " + this.Cin + Environment.NewLine +
+                   "Nom : " + this.Nom + Environment.NewLine +
+                   "Prénom : " + this.Nom + Environment.NewLine +
+                   "Téléphone : " + this.Nom + Environment.NewLine ;
+        }
+
+
+    }
+}
---
+++ Composants d'interface/Interface Desktop/Développer en C#/POO/2 - La classe Compte/Exercice_2/Exercice_2/Compte.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice_2
+{
+    class Compte
+    {
+        static int compteur;
+
+        public Client CompteClient { get;}
+        public int Solde { get; private set; }
+
+        public Compte(Client compteClient, int solde, int compteur)
+        {
+            CompteClient = compteClient;
+            Solde = solde;
+            compteur++;
+        }
+
+        public int Crediter(int montant)
+        {
+            int montant;
+            bool conversionReussie;
+            do 
+            {
+                Console.WriteLine("Veuillez entrer la montant à créditer :");
+                conversionReussie = int.TryParse(Console.ReadLine(), out montant);
+            }while (!conversionReussie || montant != 0);
+            this.Solde += this.Solde + montant;
+            return this.Solde;    
+        }
+
+        public string Crediter(int Crediter, Compte Solde)
+        {
+            this.Crediter();
+        }
+
+    }
+}
