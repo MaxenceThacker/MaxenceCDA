e50cd6d9b2c576c92625ee03b16edd549d4a278e
Maxence Thacker <maxence.thacker@hotmail.fr>
2021-11-18 12:20:14 +0100

    18/11/2021 12:20:11,54

--- ComposantsInterface/Desktop/C#/Interface/Gestion de produits/Gestion_de_produits/Gestion_de_produits/MainWindow.xaml.cs
+++ ComposantsInterface/Desktop/C#/Interface/Gestion de produits/Gestion_de_produits/Gestion_de_produits/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,54 @@ namespace Gestion_de_produits
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Produits> liste;
+        string path = @"../../Produits.json";
+
         public MainWindow()
         {
             InitializeComponent();
+            liste = TransformeJson();
+            RemplirGrid();
+            //CreerFichier();
+        }
+        public void RemplirGrid()
+        {
+            listeProduits.ItemsSource = liste;
+        }
+        private void CreerFichier()
+        {
+            File.WriteAllText(path, JsonConvert.SerializeObject(liste, Formatting.Indented));
+        }
+
+        private string LireFichier()
+        // Renvoi un tableau de chaine contenant les informations stockées dans le fichier 
+        {
+            string chaine ;
+            try
+            {
+                // Lecture et stockage dans chaine
+                chaine = File.ReadAllText(path);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Une exception s'est produite : " + e.Message);
+                Console.WriteLine("Indiquer le path :");
+                path = Console.ReadLine();
+                chaine = LireFichier();
+            }
+            return chaine;
+        }
+
+        private List<Produits> TransformeJson()
+        {
+            string chaine = LireFichier();
+            List<Produits> liste = JsonConvert.DeserializeObject<List<Produits>>(chaine);
+            return liste;
+        }
+
+        private void listeProduits_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
