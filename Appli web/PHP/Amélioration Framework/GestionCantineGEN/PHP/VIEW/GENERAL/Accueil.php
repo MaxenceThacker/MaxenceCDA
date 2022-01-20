9ab85d938d4c8c80e64fcd74c6c70541e1aafc40
MaxenceThacker <maxence.thacker@hotmail.fr>
2022-01-20 17:00:55 +0100

    Ajout Accueil

---
+++ Appli web/PHP/Amélioration Framework/GestionCantineGEN/PHP/VIEW/GENERAL/Accueil.php
@@ -0,0 +1,11 @@
+<div class="demiPage colonne center">
+        <a href="index.php?page=ListeEleves"><?php echo 'Eleves' ?></a>
+        <div></div>
+        <a href="index.php?page=ListePaiements"><?php echo 'Paiements' ?></a>
+        <div></div>
+        <a href="index.php?page=ListeMenus"><?php echo 'Menus' ?></a>
+        <div></div>
+        <a href="index.php?page=ListeModesDePaiement"><?php echo 'ModeDesPaiement' ?></a>
+        <div></div>
+    </div>
+</div>
