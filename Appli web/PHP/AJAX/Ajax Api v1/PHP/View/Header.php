046dd771279847aea2e448178bd2a81fc77289c8
59011-07-11 <59011-07-11@exchange.ad.afpanet>
2022-01-24 17:36:16 +0100

    API Biblio

--- Appli web/PHP/AJAX/Ajax Api v1/PHP/View/Header.php
+++ Appli web/PHP/AJAX/Ajax Api v1/PHP/View/Header.php
@@ -3,9 +3,12 @@
 	<div id="page">
 		<div id="entete">
 			<div id="titre">
-				
+				<div id="logo">
+			 	<img src="Images/logo.png" />
+				</div>
+
 				<div id="titre_page">
-                  Etudiant et Bibliotheque
+                  Api Fleurs
                 </div>
             </div>
 		</div>
--- Appli web/PHP/AJAX/BiblioEtudiante/JS/ajax.js
+++ Appli web/PHP/AJAX/BiblioEtudiante/JS/ajax.js
@@ -3,13 +3,15 @@ var enregs;
 
 var req = new XMLHttpRequest();
 
-req.open("GET", "https://www.data.gouv.fr/fr/datasets/liste-publique-des-organismes-de-formation-l-6351-7-1-du-code-du-travail/", true);   
+req.open("GET", "https://opendata.paris.fr/api/records/1.0/search/?dataset=tous-les-documents-des-bibliotheques-de-pret&q=&facet=langue&facet=editeur&facet=auteur_nom&facet=auteur_secondaire_nom&facet=categorie_statistique_1&facet=categorie_statistique_2&facet=auteur&facet=collectivite_auteur", true);   
 req.send(null);
+
+
 
 req.onreadystatechange = function (event) {
     if (this.readyState == XMLHttpRequest.DONE) {
         if (this.status == 200) /*Code de validité */ {
-            reponse = JSON.parse(req.responseText);
+            reponse = JSON.parse(this.responseText);
             console.log(this.responseText);
             console.log(reponse);
             enregs = trie(reponse.records);
@@ -19,10 +21,10 @@ req.onreadystatechange = function (event) {
                 ligne.setAttribute("class", "ligne");
                 ligne.id = i;
                 nombre_d_exemplaires = document.createElement("div");
-                nombre_d_exemplaires.setAttribute("class", "commune");
+                nombre_d_exemplaires.setAttribute("class", "nombre_d_exemplaires");
                 ligne.appendChild(nombre_d_exemplaires);
                 titre = document.createElement("div");
-                titre.setAttribute("class", "nom");
+                titre.setAttribute("class", "titre");
                 ligne.appendChild(titre);
                 date = document.createElement("div");
                 date.setAttribute("class", "date");
@@ -35,10 +37,30 @@ req.onreadystatechange = function (event) {
                 nombre_d_exemplaires.innerHTML = enregs[i].fields.nombre_d_exemplaires;
                 titre.innerHTML = enregs[i].fields.titre;
                 date.innerHTML = enregs[i].fields.date;
+            }
+            console.log("Réponse reçue: %s", this.responseText)
+        } else {
+            console.log("Status de la réponse: %d (%s)", this.status, this.statusText);
+        }
+        
+    }
+};
 
-                // on ajoute un evenement sur ligne pour afficher le detail
-                ligne.addEventListener("click", afficheDetail);
+function trie(tab){
+
+    do{
+        //on part du principe que le tableau est bien trié, et dès que l'on fait une permutation il ne l'est plus. Si on ne fait plus de permutation alors il est trié et on sort de la boucle
+        flag=true;
+        //on compare tout les éléments du tableau avec son voisin (sauf le dernier)
+        for(let i=0;i<tab.length-1;i++){
+            //si la valeur suivante est plus petite alors on permute les 2 valeurs dans le tableau
+            if(tab[i+1].fields.commune<tab[i].fields.commune){
+                temp=tab[i];
+                tab[i]=tab[i+1];
+                tab[i+1]=temp;
+                flag=false;    
             }
         }
-    }
+    }while(!flag);
+    return tab;
 }
