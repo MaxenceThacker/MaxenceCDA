f6686b4fad31d7dc101865ba166c615b8b5643ae
MaxenceThacker <maxence.thacker@hotmail.fr>
2022-01-26 10:33:44 +0100

    changement script.js

--- Appli web/PHP/AJAX/API INTERNE/GalerieArt/JS/script.js
+++ Appli web/PHP/AJAX/API INTERNE/GalerieArt/JS/script.js
@@ -9,17 +9,8 @@ req.onreadystatechange = function (event) {
         if (this.status == 200) {
             reponse = JSON.parse(this.responseText);
             countOeuvres.innerHTML = "";
-            for (let i = 0; i < reponse.length; i++) {
-                ligne = document.createElement("div");
-                ligne.setAttribute("class", "ligne");
-                ligne.id = i;
-                oeuvre = document.createElement("div");
-                oeuvre.setAttribute("class", "uneOeuvre");
-                oeuvre.setAttribute("id", idOeuvre);
-                listeOeuvre.appendChild(oeuvre);
-                oeuvre.innerHTML = count(reponse[i].typeOeuvre + ' : ' + reponse[i].titreOeuvre);
-                
-            }
+            oeuvre.innerHTML = count(reponse.typeOeuvre + ' : ' + reponse.titreOeuvre);
+            select.addEventListener("change", changeOeuvre);
         }
     }
 };
@@ -30,6 +21,6 @@ function changeOeuvre(){
     req.open('GET', 'index.php?page=ListeAuteursOeuvresAPI', true);
     req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     var id = select.value;
-    var args = "titreOeuvre";
+    var args = "titreOeuvre" + id;
     req.send(args);
 }
